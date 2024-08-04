5588dca9a945198c89a52cc1cd359c89046a2750
Vaibhav Zodge <zodgevaibhav@gmail.com>
2024-08-04 21:06:30 +0530

    test: Test build is getting failed
    * Should fail at compilation

--- src/main/java/org/dnyanyog/ApiGatewayMain.java
+++ src/main/java/org/dnyanyog/ApiGatewayMain.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiGatewayMain {
 
   public static void main(String[] args) {
-    SpringApplication.run(ApiGatewayMain.class, args);
+    SpringApplication.run(ApiGatewayMain.class, args)
   }
 }
